296415a10d4e1a9ab1d73e165b4d918c2a09a092
Zuura Idrisova <zuuraidrisova@gmail.com>
2022-02-03 23:52:54 -0500

    Review

--- src/test/java/com/cybertek/runners/CukesRunner.java
+++ src/test/java/com/cybertek/runners/CukesRunner.java
@@ -8,7 +8,11 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 
-        plugin = "html:target/cucumber-report.html",
+        plugin = {
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json",
+                "rerun:target/rerun.txt"
+                    },
         features = "src/test/resources/features",
         glue = "com/cybertek/stepDefinitions",
         tags = "@smartBear",
--- src/test/java/com/cybertek/stepDefinitions/Hooks.java
+++ src/test/java/com/cybertek/stepDefinitions/Hooks.java
@@ -1,7 +1,11 @@
 package com.cybertek.stepDefinitions;
 
+import com.cybertek.utilities.Driver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 public class Hooks {
 
@@ -11,11 +15,33 @@ public class Hooks {
         System.out.println("---------->>>Before Scenario : setting up browser");
     }
 
+
     @After
-    public void teardownScenario(){
+    public void teardownScenario(Scenario scenario){
 
         System.out.println("---------->>>After Scenario : closing browser");
 
+        System.out.println("scenario.getName() = " + scenario.getName());
+
+        System.out.println("scenario.getSourceTagNames() = " + scenario.getSourceTagNames());
+
+        System.out.println("scenario.isFailed() = " + scenario.isFailed());
+
+        //#1 we need to take a screen shot using Selenium
+        //   getScreenshotAs(): to be able to use this method we have to cast our Driver
+        //   to (TakesScreenshot)
+        //#2 and attach it to our report
+
+        if (scenario.isFailed()) {
+
+            byte[] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+
     }
+
 
 }
